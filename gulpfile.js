c3395cee4f717c887f54d0c00347e78cb095c9fb
Caner Uguz <caneruguz@gmail.com>
2015-05-19 10:22:35 -0400

    Add temporary gulp file

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+    concat = require("gulp-concat"),
+    minifyCSS = require('gulp-minify-css'),
+    less = require('gulp-less'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify');
+
+
+//var paths = {
+//    cssfiles : [
+//        //"./bower_components/bootstrap/dist/css/*.min.css",
+//        "./css/*.css"
+//    ],
+//    jsfiles : [
+//        "./bower_components/jquery/dist/*.min.js",
+//        "./bower_components/jquery-ui/*.min.js",
+//        "./bower_components/jquery-mockjax/*.js",
+//        "./bower_components/bootstrap/dist/js/*.min.js",
+//        "./js/style.js"
+//    ],
+//    json : "./sample.json",
+//    less : "./less/*.less"
+//};
+//
+//
+//gulp.task('js', function(){
+//    return gulp.src(paths.jsfiles)
+//        .pipe(concat('bundle.js'))
+//        .pipe(gulp.dest('./demo/'));
+//});
+//
+//gulp.task('watch', function () {
+//    gulp.watch(paths.less, ['css', 'css-dist-min']);
+//    gulp.watch(paths.jsfiles, ['js', 'js-dist-min', 'js-dist-full']);
+//});
+
+
+
+gulp.task("default", ["watch"]);
